050f5dab8784d051ff801b5c624aa7ff8a7c8dad
Edward Norton <edwardnorton@icloud.com>
2018-01-11 22:02:23 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,3 +10,18 @@ def turn(board)
   puts "Please enter 1-9:"
 end
 
+def valid_move?(board, index)
+  if index.between?(1,9)
+    if !position_taken?(board, index) 
+      true
+      end
+      end
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil 
+    taken = false 
+  else taken = true 
+  end
+end
