460309e324b547f6314e894399d4d35f2dcf6dff
Edward Norton <edwardnorton@icloud.com>
2018-01-11 22:19:45 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -22,8 +22,8 @@ def valid_move?(board, index)
   if index.between?(0,8)
     if !position_taken?(board, index) 
       true
-      end
-      end
+    end
+  end
   index.between?(0,8) && !position_taken?(board, index)
 end
 
